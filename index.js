b4c7da6ef416855641861108772fbfd947673991
Javier <javiergonzalez13a@gmail.com>
2023-04-24 15:34:45 +0200

    Buscador funcionando

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 import express from "express";
 import stackOverflowController from "./controllers/stackOverflowController.js";
+import path from "path";
 
 const app = express();
 
 app.get("/", async (req, res) => {
-    try{
-        const  query  = req.query.q;
-        const { title, question, answers } = await stackOverflowController.getContent(query);
-        res.json({ title, question, answers });
+  try {
+    const __dirname = path.resolve();
+    res.sendFile(path.join(__dirname, "index.html"));
+  } catch (error) {
+    res.status(500).send("Hubo un error al cargar la página de Stack Overflow :(");
+  }
+});
 
-    } catch (error) {
-        //throw new Error(error);
-        res.status(500).send("Hubo un error");
-    }
-        
+app.get("/api", async (req, res) => {
+  try {
+    const query = req.query.q;
+    const { title, question, answers } = await stackOverflowController.getContent(query);
+    res.json({ title, question, answers });
+  } catch (error) {
+    res.status(500).send("Hubo un error al cargar la página de Stack Overflow :(");
+  }
 });
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  console.log("Server running on port 3000");
 });
-
- 
--- utils/parser.js
+++ utils/parser.js
@@ -19,15 +19,33 @@ class Parser {
     this.loadDocument();
   }
 
+  /**
+   * Carga el documento HTML en el DOM
+   * @method
+   * @private
+   */
+
   loadDocument() {
     const JSDOM = jsdom.JSDOM;
     const dom = new JSDOM(this.html);
     this.document = dom.window.document;
   }
 
+  /**
+   * Obtiene el título de la pregunta
+   * @method
+   * @returns {string} - Título de la pregunta
+   */
+
   getQuestionAsDOM() {
     return this.document.querySelector('.question');
   }
+
+  /**
+   * Obtiene el título de la pregunta
+   * @method
+   * @returns {string} - Título de la pregunta
+   */
 
   getQuestion() {
     const question = this.getQuestionAsDOM();
@@ -44,51 +62,78 @@ class Parser {
     };
   }
 
+  /**
+   * Obtiene el título de la pregunta
+   * @method
+   * @returns {string} - Título de la pregunta
+   */
+
   getTitle() {
     const title = this.document.querySelector('#question-header h1').textContent.trim();
     return title;
   }
+
+  /**
+   * Obtiene el comentario de la pregunta
+   * @method
+   * @returns {string} - Comentario de la pregunta
+   */
 
   getAsk(element) {
     const ask = element.querySelector('.user-info .relativetime').textContent.trim();
     return ask;
   }
 
+  /**
+   * Obtiene el número de votos de la pregunta
+   * @method
+   * @returns {number} - Número de votos de la pregunta
+   * @example
+   */
+
   getVotesQuestion() {
     const votes = this.document.querySelector('.js-vote-count').textContent.trim();
     return parseInt(votes);
   }
 
+  /**
+   * Obtiene los comentarios de la pregunta
+   * @method
+   * @returns {string[]} - Comentarios de la pregunta
+   */
+
   getLinks() {
-    const links = Array.from (this.document.querySelectorAll("#search a"));
+    const links = Array.from(this.document.querySelectorAll("#search a"));
     return links.map((link) => link.href);
-  } 
+  }
+
+  /**
+   * Obtiene los comentarios de la pregunta
+   * @method
+   * @returns {string[]} - Comentarios de la pregunta
+   */
 
   getComments(element) {
-    const links = Array.from (element.querySelector(".comments-list").querySelectorAll("li"));
+    const links = Array.from(element.querySelector(".comments-list").querySelectorAll("li"));
     return links.map((link) => link.textContent.trim());
-  } 
-
-  /* getLinks(element) {
-    if (!element) {
-      return [];
-    }
-  
-    const commentsList = element.querySelector('.comments-list');
-    if (commentsList) {
-      const links = Array.from(commentsList.querySelectorAll('li'));
-      return links.map((link) => link.textContent.trim());
-    } else {
-      return [];
-    }
-  } */
-  
+  }
 
 
+  /**
+   * Obtiene las respuestas de la pregunta
+   * @method
+   * @returns {string[]} - Respuestas de la pregunta
+   */
 
   getAnswerAsDOM() {
     return Array.from(this.document.querySelectorAll('.answer'));
   }
+
+  /**
+   * Obtiene las respuestas de la pregunta
+   * @method
+   * @returns {string[]} - Respuestas de la pregunta
+   */
 
   answerQuestion() {
     const answers = this.getAnswerAsDOM();
@@ -104,18 +149,28 @@ class Parser {
     });
   }
 
+  /**
+   * Obtiene el número de votos de la respuesta
+   * @method
+   * @returns {number} - Número de votos de la respuesta
+   */
+
   getVoteAnswer(element) {
     const votes = element.querySelector('.js-vote-count').textContent.trim();
     return parseInt(votes);
   }
+
+  /**
+   * Obtiene el autor de la respuesta
+   * @method
+   * @returns {string} - Autor de la respuesta
+   */
 
   getAuthors(element) {
     const author = element.querySelector('.user-details a').textContent.trim();
     return author;
   }
 
-
-  
 }
 
 export default Parser;
